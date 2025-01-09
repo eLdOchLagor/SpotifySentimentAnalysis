243a8ca908b394b8b204e9b256dc4446c5feefac
Nikita Sidarovich <94785889+LionF4ng@users.noreply.github.com>
2025-01-09 14:03:14 +0100

    Det funkar v3 :)
    🐽

--- main.py
+++ main.py
@@ -4,11 +4,9 @@ import base64
 from requests import post, get
 import json
 import lyricsgenius
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from transformers import pipeline
 from sentence_transformers import SentenceTransformer
 from sklearn.cluster import DBSCAN
-import numpy as np
 
 # Behöver installeras via pip: lyricsgenius, requests, python-dotenv, vaderSentiment
 
@@ -62,32 +60,6 @@ def get_songs_from_playlist(token, playlist_link,number_of_songs = 20):
 
     return json_result
 
-#VADER
-# Function to print sentiments of the sentence.
-def sentiment_scores(sentence):
-
-    # Create a SentimentIntensityAnalyzer object.
-    sid_obj = SentimentIntensityAnalyzer()
-
-    # polarity_scores method of SentimentIntensityAnalyzer object gives a sentiment dictionary.
-    # which contains pos, neg, neu, and compound scores.
-    sentiment_dict = sid_obj.polarity_scores(sentence)
-    
-    print("Overall sentiment dictionary is : ", sentiment_dict)
-    print("Sentence was rated as ", sentiment_dict['neg']*100, "% Negative")
-    print("Sentence was rated as ", sentiment_dict['neu']*100, "% Neutral")
-    print("Sentence was rated as ", sentiment_dict['pos']*100, "% Positive")
-
-    print("Sentence Overall Rated As", end=" ")
-
-    # Decide sentiment as positive, negative, or neutral
-    if sentiment_dict['compound'] >= 0.05 :
-        print("Positive")
-    elif sentiment_dict['compound'] <= -0.05 :
-        print("Negative")
-    else :
-        print("Neutral")
-    return sentiment_dict['compound']
 
 def remove_repeated_sections_advanced(lyrics):
     lines = [line.strip() for line in lyrics.split("\n") if line.strip()]
@@ -110,9 +82,11 @@ def remove_repeated_sections_advanced(lyrics):
             seen_clusters.add(cluster)
     
     return "\n".join(unique_lines)
-
+        
 token = get_token()
-result = get_songs_from_playlist(token, "https://open.spotify.com/playlist/6GTsKH1x2qYJzAel3LcESW?si=13d4a1d361684328")
+happy_ex = "https://open.spotify.com/playlist/6GTsKH1x2qYJzAel3LcESW?si=13d4a1d361684328"
+sad_ex = "https://open.spotify.com/playlist/5vsVEsAqzKWbHXHbk4uL7S?si=0fa2863970b048ea"
+result = get_songs_from_playlist(token, sad_ex, 5)
 
 # Configure Genius
 genius = lyricsgenius.Genius(genius_token,timeout=30)
@@ -144,13 +118,11 @@ print("All lyrics saved to lyrics.json")
 bert = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
 
 playlist_mood = 0
-vader_score = 0
 for item in all_lyrics:
     lyrics_processed = remove_repeated_sections_advanced(item["lyrics"])
-    vader_score = sentiment_scores(lyrics_processed)
     bert_result = bert(lyrics_processed)[0]
     bert_score = bert_result["score"] if bert_result["label"] == "POSITIVE" else -bert_result["score"]
-    playlist_mood += 0*vader_score + 1*bert_score
+    playlist_mood += bert_score
 
 playlist_mood = playlist_mood/len(all_lyrics)
 print(playlist_mood)
